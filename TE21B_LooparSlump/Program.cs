1948c0855732fb614a3020e3ade63e75c745327f
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-09-29 15:17:17 +0200

    Initial commit

---
+++ TE21B_LooparSlump/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+int mickeHp = 10;
+int martinHp = -6;
+
+Console.WriteLine(mickeHp > 0 && martinHp > 0);
+
+while (mickeHp > 0 && martinHp > 0)
+{
+
+}
+
+// int x = 0;
+
+// while (x < 8)
+// {
+//   Console.WriteLine(x);
+// }
+
+// x++;
+
+
+Console.ReadLine();
+
+// Random generator = new Random();
+
+// int hitRoll = generator.Next(100);
+// Console.WriteLine(hitRoll);
+
+// if (hitRoll > 95)
+// {
+//   Console.WriteLine("CRIT!");
+// }
+// else if (hitRoll > 40)
+// {
+//   Console.WriteLine("HIT!");
+// }
+// else
+// {
+//   Console.WriteLine("MISS!");
+// }
